a34193690d2465765bc0938197ee54c7fb0f2e69
IndoorDragon <IndoorDragonCoco@gmail.com>
2025-11-04 20:34:55 -0500

    Halfway through Building a Graph.  I get ths gist of it, I will return for shaders tutorial when needed.

---
+++ 02-02_Basics/01_Building_a_Graph/Assets/Scripts/Graph.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab;
+
+    void Awake () {
+        //Vector3 position;
+        var position = Vector3.zero;
+        var scale = Vector3.one / 5f;
+        for (int i = 0; i < 10; i++) {
+            Transform point = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) / 5f - 1f;
+            position.y = position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+        }
+    }
+}
